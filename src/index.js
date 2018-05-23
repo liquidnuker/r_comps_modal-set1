af6bd74ce44b4fc7a20bd3d30e38955b143f24ca
administrator <liquidnuker@users.noreply.github.com>
2018-05-22 19:05:05 -0700

    added default modal with no animation

--- src/index.js
+++ src/index.js
@@ -16,3 +16,19 @@ let modal1 = new BS4Modal1({
 });
 
 modal1.init();
+
+let modal2 = new BS4Modal1({
+  modalBackground: "bs4modal2_bg",
+  modalContent: "bs4modal2-content",
+  showBtn1: "bs4modal2_show",
+  showBtn2: "", // optional
+  closeBtn1: "bs4modal2_closebtn1",
+  closeBtn2: "", // optional
+  // animateEntry: true, // optional
+  animateEntryClass: "animated bounceIn",
+  // animateExit: true, // optional
+  animateExitClass: "animated bounceOut"
+});
+
+modal2.init();
+
